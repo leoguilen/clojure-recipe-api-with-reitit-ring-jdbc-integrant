7a6d524d219308115b08a21360f3319b71b51909
Leonardo C. Guilen <leonardo.guilen@outlook.com.br>
2025-01-04 20:34:20 +0000

    chore: configure project.clj with dev profile and dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,19 @@
 (defproject cheffy "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :main ^:skip-aot cheffy.core
-  :target-path "target/%s"
+  :description "Cheffy REST API"
+  :url "https://github.com/leoguilen/clojure-recipe-rest-api"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring "1.8.1"]
+                 [integrant "0.8.0"]
+                 [environ "1.2.0"]
+                 [metosin/reitit "0.5.5"]
+                 [seancorfield/next.jdbc "1.1.582"]
+                 [org.postgresql/postgresql "42.2.14"]
+                 [clj-http "3.10.0"]
+                 [ovotech/ring-jwt "1.3.0"]]
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev {:source-paths ["dev/src"]
+                   :resources-paths ["dev/resources"]
+                   :dependencies [[ring/ring-mock "0.4.0"]
+                                  [integrant/repl "0.3.1"]]}}
+  :uberjar-name "cheffy.jar")
